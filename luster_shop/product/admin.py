2163f65c872e39ea844eec350d09538081b29c2f
Amin <asaa47721@gmail.com>
2019-12-16 23:06:29 +0330

    define Category and Product models. makemigration. add them to admin. install pillow

--- luster_shop/product/admin.py
+++ luster_shop/product/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.db import models
+from django.forms import TextInput
+from .models import Category, Product
 
-# Register your models here.
+
+# Category
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'position']
+    search_fields = ['name']
+    list_editable = ['position']
+
+
+# Product
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size':100})},
+    }
+
+    fields = ['name', 'slug', 'description', 'image', 'price', 'quantity',
+        'category', 'model', 'code', 'publish']
+    list_display = ['name', 'slug', 'price', 'quantity',
+        'category', 'model', 'code', 'publish']
+    list_filter = ['category', 'publish']
+    search_fields = ['name', 'description']
+    raw_id_fields = ['category']
+    prepopulated_fields = {'slug': ['name']}
--- luster_shop/product/models.py
+++ luster_shop/product/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    position = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['position']
+        verbose_name_plural = _('Categories')
+
+    def __str__(self):
+        return self.name
+
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(default='')
+    image = models.ImageField(upload_to='product_images',
+        default='default.jpg')
+    price = models.FloatField(default=0)
+    quantity = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL,
+        null=True)
+    model = models.CharField(max_length=40, blank=True)
+    code = models.CharField(max_length=40, blank=True)
+    publish = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['-id']
+    
+    def __str__(self):
+        return self.name
