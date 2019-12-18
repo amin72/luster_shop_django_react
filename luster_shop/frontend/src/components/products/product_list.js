789b223b174f4212d7749c64d744fe5ba54bea38
Amin <asaa47721@gmail.com>
2019-12-18 22:56:16 +0330

    get products and show them in product_list.js. add main.css to index.html

--- luster_shop/frontend/src/components/products/product_list.js
+++ luster_shop/frontend/src/components/products/product_list.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
@@ -17,8 +17,35 @@ class ProductList extends Component {
 
 
     render() {
+        const products = this.props.products.results
+        let product_results = null
+
+        if (products) {
+            product_results = products.map(product => {
+                const key = product.name + product.model + product.code
+                return (
+                    <div key={key} className="col-lg-3 col-md-4 my-4 border-5 px-4">
+                        <div className="row">
+                            <a href="#">
+                                <img className="mx-auto img-fluid img-thumbnail rounded" src={ product.image } alt={product.name} />
+                            </a>
+                        </div>
+                        <h4 className="my-1 product-name" style={{ height: "50px", overflow: "hidden", fontSize: "16px" }}><a href="#">{ product.name }</a></h4>
+                        <span className="mt-5"><strong>{ product.price }</strong></span>
+                    </div>
+                )
+            })
+        }
+
         return (
-            <h1>Product List</h1>
+            <Fragment>
+                <h1>Product List</h1>
+                <div className="row">
+                {
+                    products && product_results ? product_results : null
+                }
+                </div>
+            </Fragment>
         )
     }
 }
