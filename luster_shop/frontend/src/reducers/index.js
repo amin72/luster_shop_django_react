9ea5091ba477c340b30475c43f54173bae84ec3a
Amin <asaa47721@gmail.com>
2019-12-31 00:42:51 +0330

    define errors and messages reducers. add them to index reducer

--- luster_shop/frontend/src/reducers/index.js
+++ luster_shop/frontend/src/reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux'
 import products from './products'
 import auth from './auth'
+import errors from './errors'
+import messages from './messages'
 
 
 export default combineReducers({
     products,
     auth,
+    errors,
+    messages,
 })
